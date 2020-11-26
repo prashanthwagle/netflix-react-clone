ad6959b5bee313f481505d979f5ac9582ed84a8b
Prashanth Wagle <prashanthwagle360@gmail.com>
2020-11-27 01:08:08 +0530

    Connected with Firebase and added Context and Lib

---
+++ src/context/firebase.js
@@ -0,0 +1,4 @@
+const { createContext } = require("react");
+import { createContext } from "react";
+
+export const firebaseContext = createContext(null);
--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import App from "./App";
 import { GlobalStyles } from "./global-styles";
 import "normalize.css";
 
+import { firebase } from "./lib/firebase.prod";
+import { FirebaseContext } from "./context/firebase";
+
 render(
   <>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={firebase}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </>,
   document.getElementById("root")
 );
