7ace487b2a1c67e86fa1b3871cb5a4eaa04cd538
Prashanth Wagle <prashanthwagle360@gmail.com>
2020-12-03 12:35:39 +0530

    Added ProtectedRoutes adn IsUserRedirect to all Routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Home, SignIn } from "./pages";
+import { Home, SignIn, SignUp, Browse } from "./pages";
 import * as ROUTES from "./constants/routes";
+import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
 
 export default function App() {
+  const user = {};
   return (
     <Router>
-      <Route exact path={ROUTES.HOME}>
-        <Home />
-      </Route>
-      <Route exact path={ROUTES.SIGN_IN}>
+      <IsUserRedirect
+        user={user}
+        path={ROUTES.SIGN_IN}
+        loggedInPath={ROUTES.BROWSE}
+      >
         <SignIn />
-      </Route>
+      </IsUserRedirect>
+      <IsUserRedirect
+        user={user}
+        path={ROUTES.SIGN_UP}
+        loggedInPath={ROUTES.BROWSE}
+      >
+        <SignUp />
+      </IsUserRedirect>
+      <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+        <Browse />
+      </ProtectedRoute>
+      <IsUserRedirect
+        user={user}
+        loggedInPath={ROUTES.BROWSE}
+        path={ROUTES.HOME}
+      >
+        <Home />
+      </IsUserRedirect>
       <Route exact path="/test">
         <p>Testing....123</p>
       </Route>
