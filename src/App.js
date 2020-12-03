996d58b731bc527e1bd4838d6058106fc73aff18
Prashanth Wagle <prashanthwagle360@gmail.com>
2020-12-04 01:57:22 +0530

    Added Switch and made use of useAthListener

--- src/App.js
+++ src/App.js
@@ -1,40 +1,44 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Home, SignIn, SignUp, Browse } from "./pages";
 import * as ROUTES from "./constants/routes";
 import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
+import { useAuthListener } from "./hooks";
 
 export default function App() {
-  const user = {};
+  const { user } = useAuthListener();
+  console.log(user);
   return (
     <Router>
-      <IsUserRedirect
-        user={user}
-        path={ROUTES.SIGN_IN}
-        loggedInPath={ROUTES.BROWSE}
-      >
-        <SignIn />
-      </IsUserRedirect>
-      <IsUserRedirect
-        user={user}
-        path={ROUTES.SIGN_UP}
-        loggedInPath={ROUTES.BROWSE}
-      >
-        <SignUp />
-      </IsUserRedirect>
-      <ProtectedRoute user={user} path={ROUTES.BROWSE}>
-        <Browse />
-      </ProtectedRoute>
-      <IsUserRedirect
-        user={user}
-        loggedInPath={ROUTES.BROWSE}
-        path={ROUTES.HOME}
-      >
-        <Home />
-      </IsUserRedirect>
-      <Route exact path="/test">
-        <p>Testing....123</p>
-      </Route>
+      <Switch>
+        <IsUserRedirect
+          user={user}
+          path={ROUTES.SIGN_IN}
+          loggedInPath={ROUTES.BROWSE}
+        >
+          <SignIn />
+        </IsUserRedirect>
+        <IsUserRedirect
+          user={user}
+          path={ROUTES.SIGN_UP}
+          loggedInPath={ROUTES.BROWSE}
+        >
+          <SignUp />
+        </IsUserRedirect>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse />
+        </ProtectedRoute>
+        <IsUserRedirect
+          user={user}
+          loggedInPath={ROUTES.BROWSE}
+          path={ROUTES.HOME}
+        >
+          <Home />
+        </IsUserRedirect>
+        <Route exact path="/test">
+          <p>Testing....123</p>
+        </Route>
+      </Switch>
     </Router>
   );
 }
