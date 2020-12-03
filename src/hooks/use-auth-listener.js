c5d1a271adf351cea5e4b3b84173160a5f6b2247
Prashanth Wagle <prashanthwagle360@gmail.com>
2020-12-04 01:57:37 +0530

    Added useAuthListener Custom Hook

---
+++ src/hooks/use-auth-listener.js
@@ -0,0 +1,26 @@
+import { auth } from "firebase";
+import React, { useState, useEffect, useContext } from "react";
+import { FirebaseContext } from "../context/firebase";
+function useAuthListener() {
+  const { firebase } = useContext(FirebaseContext);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const listener = firebase.auth().onAuthStateChanged(function (authUser) {
+      if (authUser) {
+        localStorage.setItem("authUser", JSON.stringify(authUser));
+        setUser(authUser);
+      } else {
+        localStorage.removeItem("authUser");
+      }
+    });
+
+    return () => {
+      listener();
+    };
+  }, []);
+
+  return { user };
+}
+
+export default useAuthListener;
