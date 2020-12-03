f2ede094721503ede8809be33f06f69be6d124ab
Prashanth Wagle <prashanthwagle360@gmail.com>
2020-12-04 01:59:02 +0530

    Added Browse Component

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { useContent } from "../hooks";
+import { selectionFilter } from "../utils";
 
 function Browse() {
+  const { films } = useContent("films");
+  const { series } = useContent("series");
+  const slides = selectionFilter({ series, films });
+
   return <div>Browse Page</div>;
 }
 
